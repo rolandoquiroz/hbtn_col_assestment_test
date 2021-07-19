9f715662128d7c843b8708760668e60bfecec983
rolandoquiroz <rolandoquiroz@gmail.com>
2021-07-19 17:36:40 -0500

    HCAT-19 Add user creation api tests

--- backend/retail_company/tests/test_views.py
+++ backend/retail_company/tests/test_views.py
@@ -14,32 +14,32 @@ class CreateNewUserTest(TestCase):
     """ Test module for inserting a new user """
     def setUp(self):
         self.valid_payload = {
-            'name': 'Mary',
-            'last_name': 'Mena',
-            'government_issued_id': "654321",
-            "email": "mary@dea.com",
-            "company": "DEA"
+            "name": "Mary",
+            "last_name": "Mena",
+            "government_issued_id": "654321",
+            "email": "mary@example.com",
+            "company": ""
         }
         self.invalid_payload = {
-            'name': '',
-            'last_name': 'Lopez',
-            'government_issued_id': "123456",
+            "name": "",
+            "last_name": "Lopez",
+            "government_issued_id": "123456",
             "email": "peter@it.com",
             "company": "It company"
         }
 
     def test_create_valid_user(self):
         response = client.post(
-            reverse('post_user'),
+            reverse("post_user"),
             data=json.dumps(self.valid_payload),
-            content_type='application/json'
+            content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_invalid_user(self):
         response = client.post(
-            reverse('post_user'),
+            reverse("post_user"),
             data=json.dumps(self.invalid_payload),
-            content_type='application/json'
+            content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
