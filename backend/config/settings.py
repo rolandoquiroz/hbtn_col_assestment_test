9bfcde7242c0e26c5cbb3c65b8c905ba4d7a9468
rolandoquiroz <rolandoquiroz@gmail.com>
2021-07-22 10:34:13 -0500

    HCAT-30 Set token authentication.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^#!2!ak1s+^w&rhp=db8o+_*)@w0s19u8q2!-(--*qfn$+b5us'
+SECRET_KEY = \
+    'django-insecure-^#!2!ak1s+^w&rhp=db8o+_*)@w0s19u8q2!-(--*qfn$+b5us'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     # 3rd party
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
 
     # Local
     'retail_company'
@@ -96,16 +99,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+            UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+            MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+            CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+            NumericPasswordValidator',
     },
 ]
 
@@ -135,8 +142,14 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-        ],
+    'DEFAULT_PERMISSION_CLASSES':
+        [
+            'rest_framework.authentication.TokenAuthentication',
+            ],
+    'DEFAULT_AUTHENTICATION_CLASSES':
+        [
+            'rest_framework.authentication.SessionAuthentication',
+            'rest_framework.authentication.TokenAuthentication'
+            ],
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,17 +1,27 @@
 from django.contrib import admin
-from django.conf.urls import include, url
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import include, path
 
 
 urlpatterns = [
-    url(
-        r'^admin/',
-        admin.site.urls
+    path(
+        route='admin/',
+        view=admin.site.urls,
+        name='admin',
         ),
-    url(r'^',
-        include('retail_company.urls')
+    path(
+        route='api-auth/',
+        view=include('rest_framework.urls'),
+        name='rest_framework',
         ),
-    url(
-        r'^api-auth/',
-        include('rest_framework.urls', namespace='rest_framework')
-    ),
+    path(
+        route='api/v1/',
+        view=include('retail_company.urls'),
+        name='api-v1',
+        ),
+    path(
+        route='api/v1/dj-rest-auth/',
+        view=include('dj_rest_auth.urls'),
+        name='dj-rest-auth',
+        ),
 ]
