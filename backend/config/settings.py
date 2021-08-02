52a7086b96b8144ed7f0d658091ecca5bc0cc567
rolandoquiroz <rolandoquiroz@gmail.com>
2021-08-02 14:52:43 -0500

    HCAT-42 Add running User API with AllowAny permissions

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -158,9 +158,9 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-        ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
