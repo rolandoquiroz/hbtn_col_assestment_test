ef375d5727ff4bd7a6fef2cf410fa1a40bb93b86
rolandoquiroz <rolandoquiroz@gmail.com>
2021-08-29 05:43:58 -0500

    HCAT-62 Set production enviroment ready to deploy

--- backend/backend/config/settings.py
+++ backend/backend/config/settings.py
@@ -6,7 +6,6 @@ from corsheaders.defaults import default_headers
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -23,7 +22,6 @@ ALLOWED_HOSTS.extend(
         os.environ.get('ALLOWED_HOSTS', '').split(','),
     )
 )
-
 
 # Application definition
 
