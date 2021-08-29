6839a2540a2125d0f3cec22392b05d05000b411c
rolandoquiroz <rolandoquiroz@gmail.com>
2021-08-29 05:03:34 -0500

    HCAT-60 Set development environment

--- backend/backend/config/settings.py
+++ backend/backend/config/settings.py
@@ -100,11 +100,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'db',
+        'PORT': '5432',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'db',
-        'PORT': '5432',
     }
 }
 
